a0c470406e1a82e3e68cf1566916339e1436d4fc
yonycherkos <yonycherkos@gmail.com>
2019-07-15 12:12:12 +0300

    adding warp triangle function

--- faceSwap.py
+++ faceSwap.py
@@ -79,6 +79,36 @@ def applyAffineTransform(src, srcTri, dstTri, size) :
     return dst
 
 
+def warpTriangle(img1, img2, t1, t2) :
+
+    # Find bounding rectangle for each triangle
+    r1 = cv2.boundingRect(np.float32([t1]))
+    r2 = cv2.boundingRect(np.float32([t2]))
+
+    # Offset points by left top corner of the respective rectangles
+    t1Rect = []
+    t2Rect = []
+
+    for i in range(3):
+        t1Rect.append(((t1[i][0] - r1[0]),(t1[i][1] - r1[1])))
+        t2Rect.append(((t2[i][0] - r2[0]),(t2[i][1] - r2[1])))
+
+    # Get mask by filling triangle
+    mask = np.zeros((r2[3], r2[2], 3), dtype = np.float32)
+    cv2.fillConvexPoly(mask, np.int32(t2Rect), (1.0, 1.0, 1.0));
+
+    # Apply warpImage to small rectangular patches
+    img1Rect = img1[r1[1]:r1[1] + r1[3], r1[0]:r1[0] + r1[2]]
+
+    size = (r2[2], r2[3])
+    img2Rect = applyAffineTransform(img1Rect, t1Rect, t2Rect, size)
+    img2Rect = img2Rect * mask
+
+    # Copy triangular region of the rectangular patch to the output image
+    img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] = img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] * ( (1.0, 1.0, 1.0) - mask ) + img2Rect
+    # img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] = img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] + img2Rect
+
+
 image1 = 'images/ted_cruz.jpg'
 image2 = 'images/donald_trump.jpg'
 
