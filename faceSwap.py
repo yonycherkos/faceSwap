de9a226a484445f7a56e344f149643197364715f
yonycherkos <yonycherkos@gmail.com>
2019-07-15 11:45:41 +0300

    add landmark detection function

---
+++ faceSwap.py
@@ -0,0 +1,28 @@
+# import the necessary libraries
+import numpy as np
+import cv2
+import dlib
+
+def landmark_detection(image):
+
+    # laod an image then convert it to grey scale
+    img = cv2.imread(image)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # detect the face then find the landmarks
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+    faces = detector(img_gray)
+    for face in faces:
+        landmarks = predictor(img_gray, face)
+        landmark_points = []
+        for n in range(68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            landmark_points.append((int(x), int(y)))
+
+            cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+
+
+    return landmark_points, img
