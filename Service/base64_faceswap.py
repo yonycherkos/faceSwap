9acfb97178cad06143fbcdf2b119fc63f25ad8c7
Yared Taddese <taddeseyared@gmail.com>
2019-08-05 18:17:17 +0300

    [Improve] Use faster function to convert from numpy array image to base64 string

--- Service/base64_faceswap.py
+++ Service/base64_faceswap.py
@@ -23,25 +23,11 @@ def np_img_from_base64(image_base64):
     return np.array(image)
 
 
-def np_img_to_base64(np_img):
+def base64_from_np_img(np_img):
     # change from numpy array to base64 string(rather than writing to file and read it)
-    pil_img = Image.fromarray(cv2.cvtColor(np_img, cv2.COLOR_BGR2RGB))
-    buff = io.BytesIO()
-    pil_img.save(buff, format="JPEG")
-    np_img_string = base64.b64encode(buff.getvalue()).decode("utf-8")
-    return np_img_string
-
-
-def byte_to_img(input_image):
-    binary_image = base64.b64decode(input_image)
-    file_format = magic.from_buffer(
-        base64.b64decode(input_image), mime=True).split('/')[1]
-
-    f = tempfile.NamedTemporaryFile(suffix='*.' + str(file_format))
-    f.write(binary_image)
-    image = Image.open(f.name).convert('RGB')
-
-    return image
+    retval, buffer = cv2.imencode('.jpg', np_img)
+    jpg_as_text = base64.b64encode(buffer)
+    return jpg_as_text
 
 
 def base64_face_swap(input_image, meme_image, mode):
@@ -54,10 +40,6 @@ def base64_face_swap(input_image, meme_image, mode):
     fs = FaceSwap()
     result = fs.faceSwap(inputImage, memeImage, mode=mode)
 
-    cv2.imwrite("output.jpg", result)
-
-    with open("output.jpg", 'rb') as f:
-        img = f.read()
-        image = base64.b64encode(img).decode('utf-8')
+    image = base64_from_np_img(result)
 
     return image
