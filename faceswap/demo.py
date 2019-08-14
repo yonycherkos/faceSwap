5fd122d968ebc30fd7fa012ababfaa983844cafa
Yared Taddese <taddeseyared@gmail.com>
2019-08-14 17:50:50 +0300

    [Refactor] Take Service module out on the plain and make faceswap module in subfolder [Improve] Refactor server logic code and faceswap code until delanauy triangle function [Bug Fix] Fix face alignment bug(didnot align when src face is right)

---
+++ faceswap/demo.py
@@ -0,0 +1,21 @@
+from faceswap import faceSwap
+import cv2
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Enter image path of input and meme images.')
+
+parser.add_argument('image1', help="Eneter image1 file path",
+                    type=str, default="images/yony.jpg")
+parser.add_argument('image2', help="Enter image2 file path",
+                    type=str, default="images/anchorman.jpg")
+parser.add_argument('mode', help="select either choose_largest_face or apply_on_all mode",
+                    type=str, default="images/anchorman.jpg")
+
+args = parser.parse_args()
+
+img1 = cv2.imread(args.image1)
+img2 = cv2.imread(args.image2)
+
+faceSwap(img1, img2, mode=args.mode,
+         showImages=True)
